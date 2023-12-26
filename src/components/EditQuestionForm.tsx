13b640be22703706b48a6df4790ed587b0171296
Yifeng Li <charlesliyifeng@gmail.com>
2023-12-27 12:04:10 +1300

    edit questions page

---
+++ src/components/EditQuestionForm.tsx
@@ -0,0 +1,97 @@
+import TagInput from "./TagInput";
+import Thread from "../types/Thread";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+import React, { useState } from "react";
+
+const EditQuestionForm: React.FC = () => {
+    const [title, setTitle] = useState("");
+    const [content, setContent] = useState("");
+    const [tags, setTags] = useState<string[]>([]);
+
+    function handleTitleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const inputTitle: string = event.target.value;
+        setTitle(inputTitle);
+    }
+
+    function handleContentChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const inputContent: string = event.target.value;
+        setContent(inputContent);
+    }
+
+    function handleSubmit() {
+        if (!title) {
+            alert("Your title cannot be empty");
+            return;
+        }
+        if (!content) {
+            alert("Your body cannot be empty");
+            return;
+        }
+        const newThread: Thread = {
+            id: 0,
+            title: title,
+            body: content,
+            author: "tester",
+            created_at: "",
+            updated_at: "",
+            votes: 0,
+            answers: 0,
+            accepted: false,
+            views: 0,
+            tags: tags,
+        };
+        newThread;
+    }
+
+    return (
+        <Box
+            className="centerBox"
+            sx={{ flexGrow: 1, p: 3, "& .MuiTextField-root": { m: 1, width: "100ch" } }}
+            top={80}
+        >
+            <Typography variant="h4" padding={2}>
+                Ask a question
+            </Typography>
+
+            <Typography variant="h6" display="block" padding={1}>
+                Title
+            </Typography>
+            <TextField id="title" required placeholder="Enter your question here" onChange={handleTitleChange} />
+
+            <Typography variant="h6" display="block" padding={1}>
+                Problem Details
+            </Typography>
+            <TextField
+                id="body"
+                multiline
+                required
+                rows={8}
+                placeholder="Describe the details of your question here"
+                onChange={handleContentChange}
+            />
+
+            <Typography variant="h6" display="block" padding={1}>
+                Tags
+            </Typography>
+            <TagInput tags={tags} setTags={setTags} />
+            <Box display={"flex"} flexDirection={"row"} padding={1}>
+                <Box paddingRight={2}>
+                    <Button variant="contained" onClick={handleSubmit} href="/">
+                        Submit
+                    </Button>
+                </Box>
+                <Box>
+                    <Button variant="outlined" href="/">
+                        Cancel
+                    </Button>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default EditQuestionForm;
---
+++ src/pages/EditQuestion.tsx
@@ -0,0 +1,17 @@
+import SearchBar from "../components/SearchBar";
+import SideBar from "../components/SideBar";
+import EditQuestionForm from "../components/EditQuestionForm";
+import { Box } from "@mui/material";
+import React from "react";
+
+const EditQuestion: React.FC = () => {
+    return (
+        <Box display={"flex"}>
+            <SearchBar />
+            <SideBar />
+            <EditQuestionForm />
+        </Box>
+    );
+};
+
+export default EditQuestion;
